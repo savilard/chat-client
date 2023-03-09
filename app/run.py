7b9d0dc2e336b1272892ac4021c32529e7c724cc
savilard <vitaliyantonoff@gmail.com>
2023-03-09 15:23:10 +0600

    :sparkles: Add display message to chat gui

--- app/run.py
+++ app/run.py
@@ -1,10 +1,26 @@
 import asyncio
+import time
 
 import gui
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+
+async def generate_msgs(queue: asyncio.Queue):
+    while True:
+        message = time.time()
+        queue.put_nowait(message)
+        await asyncio.sleep(1)
+
+
+async def main():
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
-    loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+
+    await asyncio.gather(
+        gui.draw(messages_queue, sending_queue, status_updates_queue),
+        generate_msgs(messages_queue),
+    )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())

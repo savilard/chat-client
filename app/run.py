38eb93bdaeebb3b3057327852567e940e1626eb2
savilard <vitaliyantonoff@gmail.com>
2023-03-09 15:45:40 +0600

    :sparkles: Add a system of settings

--- app/run.py
+++ app/run.py
@@ -1,7 +1,27 @@
 import asyncio
 import time
+from functools import wraps
+
+import anyio
+import typer
 
 import gui
+
+
+def run_async(func):
+    """Used for asynchronous run with click arguments.
+    https://github.com/tiangolo/typer/issues/88#issuecomment-889486850
+
+    Args:
+        func: decoratable asynchronous function
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        async def coro_wrapper():
+            return await func(*args, **kwargs)
+        return anyio.run(coro_wrapper)
+    return wrapper
 
 
 async def generate_msgs(queue: asyncio.Queue):
@@ -11,7 +31,42 @@ async def generate_msgs(queue: asyncio.Queue):
         await asyncio.sleep(1)
 
 
-async def main():
+@run_async
+async def main(
+    host: str = typer.Option(
+        default='minechat.dvmn.org',
+        help='Minechat host',
+        envvar='SERVER_HOST',
+    ),
+    listen_server_port: int = typer.Option(
+        default=5000,
+        help='Minechat listen port',
+        envvar='LISTEN_SERVER_PORT',
+    ),
+    writing_server_port: int = typer.Option(
+        default=5050,
+        help='Minechat writing port',
+        envvar='WRITING_SERVER_PORT',
+    ),
+    token: str = typer.Option(
+        None,
+        help='Your token',
+        envvar='MINECHAT_TOKEN',
+    ),
+    history_file_path: str = typer.Option(
+        default='minechat.history',
+        help='Path to file with history of minechat',
+        envvar='HISTORY_FILE_PATH',
+    ),
+) -> None:
+    """Entry point.
+    Args:
+        host: minechat server host
+        listen_server_port: port to receive messages
+        writing_server_port: port for sending messages
+        token: token to access the server
+        history_file_path: Path to file with history of minechat
+    """
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
@@ -23,4 +78,4 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    typer.run(main)

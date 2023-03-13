5515188f54e40de9257b711cb9a87589e5092bfa
savilard <vitaliyantonoff@gmail.com>
2023-03-13 16:45:51 +0600

    :recycle: Update logger message to watch_for_connection

--- chat_client/connection.py
+++ chat_client/connection.py
@@ -2,6 +2,7 @@ import asyncio
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
+from async_timeout import timeout
 from loguru import logger
 
 
@@ -30,6 +31,15 @@ async def watch_for_connection(queue: asyncio.Queue[str]):
     Args:
         queue: watchdog queue
     """
+    timeout_seconds = 1
     while True:
-        log_message = await queue.get()
-        logger.info(log_message)
+        async with timeout(timeout_seconds) as cm:
+            try:
+                log_message = await queue.get()
+            except (asyncio.TimeoutError, asyncio.CancelledError) as err:
+                if not cm.expired:
+                    raise err
+                logger.warning(f'{timeout_seconds}s timeout is elapsed')
+
+            else:
+                logger.info(log_message)

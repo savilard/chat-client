78896ae5e7b4b3b31e4a8b53bda0d47ad9245203
savilard <vitaliyantonoff@gmail.com>
2023-03-23 09:05:49 +0600

    :recycle: Change default value for history file path cli argument

--- chat_client/args.py
+++ chat_client/args.py
@@ -7,6 +7,6 @@ def get_args():
     parser.add_argument("--host", help="server host URL", default='minechat.dvmn.org', env_var='SERVER_HOST')
     parser.add_argument("--inport", type=int, help="port to read msgs", default=5000, env_var='PORT_IN')
     parser.add_argument("--outport", type=int, help="port to write msgs", default=5050, env_var='PORT_OUT')
-    parser.add_argument("--history", help="file to save transcript to", default='history', env_var='HISTORY_FILE_PATH')
+    parser.add_argument("--history", help="file to save transcript to", default='minechat.history', env_var='HISTORY_FILE_PATH')
     parser.add_argument("--token", help="your toke", default=None, env_var='TOKEN')
     return parser.parse_args()

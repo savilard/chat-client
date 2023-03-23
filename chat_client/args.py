699a1432ed47b44ad37c94a618ddee924506a054
savilard <vitaliyantonoff@gmail.com>
2023-03-23 10:46:46 +0600

    :recycle: Update args parser

--- chat_client/args.py
+++ chat_client/args.py
@@ -3,10 +3,10 @@ import configargparse
 
 def get_args():
     """Gets arguments from command line."""
-    parser = configargparse.ArgParser()
-    parser.add_argument("--host", help="server host URL", default='minechat.dvmn.org', env_var='SERVER_HOST')
-    parser.add_argument("--inport", type=int, help="port to read msgs", default=5000, env_var='PORT_IN')
-    parser.add_argument("--outport", type=int, help="port to write msgs", default=5050, env_var='PORT_OUT')
-    parser.add_argument("--history", help="file to save transcript to", default='minechat.history', env_var='HISTORY_FILE_PATH')
+    parser = configargparse.ArgParser(default_config_files=['settings.ini'])
+    parser.add_argument("--host", help="server host URL", default='minechat.dvmn.org')
+    parser.add_argument("--outport", type=int, help="port to read msgs", default=5000)
+    parser.add_argument("--inport", type=int, help="port to write msgs", default=5050)
+    parser.add_argument("--history", help="file to save transcript to", default='minechat.history')
     parser.add_argument("--token", help="your toke", default=None, env_var='TOKEN')
     return parser.parse_args()
